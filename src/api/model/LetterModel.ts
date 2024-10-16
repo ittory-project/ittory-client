09c6a381d278b0bb0ec0d837df1f1d848ca139b6
jrary <jrary@naver.com>
2024-10-16 22:27:41 +0900

    [RL1M-150] 현재 유저를 순서에 맞게 조회 API 연결 컴포넌트 작성

--- src/api/model/LetterModel.ts
+++ src/api/model/LetterModel.ts
@@ -1,6 +1,19 @@
 // 편지 시작 시 정보 조회(참여자) Response
-export interface LetterStartPartiGetResponse {
-    participantCount: number,
-    repeatCount: number,
-    elementCount: number
+export interface LetterStartInfoGetResponse {
+  participantCount: number,
+  repeatCount: number,
+  elementCount: number
+}
+
+// 현재 유저를 순서에 맞게 조회 
+export interface LetterPartiListGetResponse {
+  participant: LetterPartiItem
+}
+
+// 현재 유저 리스트 아이템
+export interface LetterPartiItem {
+  sequence: number,
+  memberId: number,
+  nickname: string,
+  imageUrl: string
 }
