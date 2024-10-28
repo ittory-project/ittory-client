4962554690d8f3a657c301c5fdab33b950b1ac9d
jrary <jrary@naver.com>
2024-10-28 19:47:57 +0900

    [TODO] 완료: EXIT 타입 정의, 퇴장과 메시지 작성 response 구분하기

--- src/api/model/WsModel.ts
+++ src/api/model/WsModel.ts
@@ -1,6 +1,22 @@
 export interface WsEnterResponse {
   participantId: number,
   nickname: string,
-  imageUrl: string
+  imageUrl: string,
   action: string
+}
+
+// redux에도 그대로 사용함
+export interface LetterItem {
+  elementId: string,
+  imageUrl?: string,
+  content?: string,
+  nickname?: string,
+  elementSequence?: number,
+  writeSequence?: number,
+}
+
+export interface WsExitResponse {
+  participantId: number,
+  nickname: string,
+  action: string // "EXIT"
 }
