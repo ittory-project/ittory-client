4cdd1c03e3dcdf21e34062cb0d2759dbf77d8b02
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2024-11-27 12:28:46 +0900

    [RL1M/188] 메뉴 기본이미지 설정

--- src/components/homePage/Header.tsx
+++ src/components/homePage/Header.tsx
@@ -1,10 +1,39 @@
-import React, { useState, useEffect, ReactNode } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import styled, { keyframes } from "styled-components";
+import logo from "../../../public/assets/home/smalllogo.svg";
+import menu from "../../../public/assets/home/menulogo.svg";
+import { Menu } from "../../layout/Menu";
+import { useSwipeable } from "react-swipeable";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const closeMenu = useCallback(() => {
+    setIsMenuOpen(false);
+  }, []);
+  const handleOverlayClick = useCallback(() => {
+    closeMenu();
+  }, [closeMenu]);
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => setIsMenuOpen(true),
+  });
+
+  const handleMenu = () => {
+    setIsMenuOpen(true);
+  };
   return (
     <Container>
-      <div>헤더야</div>
+      <Logo src={logo} />
+      <MenuLogo src={menu} onClick={handleMenu} />
+      {isMenuOpen && (
+        <>
+          <MenuOverlay $isOpen={isMenuOpen} onClick={handleOverlayClick} />
+          <MenuContainer $isOpen={isMenuOpen}>
+            <Menu onClose={closeMenu} />
+          </MenuContainer>
+        </>
+      )}
     </Container>
   );
 }
@@ -23,4 +52,49 @@ const Container = styled.div`
   align-items: center;
   background: #fff;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.05);
+`;
+
+const Logo = styled.img`
+  display: flex;
+  width: 58px;
+  height: 28px;
+  padding: 4px;
+  align-items: center;
+  gap: 16px;
+  flex-shrink: 0;
+`;
+
+const MenuOverlay = styled.div<{ $isOpen: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  opacity: ${(props) => (props.$isOpen ? 1 : 0)};
+  visibility: ${(props) => (props.$isOpen ? "visible" : "hidden")};
+  transition:
+    opacity 0.3s ease,
+    visibility 0.3s ease;
+  z-index: 10;
+`;
+const MenuContainer = styled.div<{ $isOpen: boolean }>`
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 260px;
+  height: 100%;
+  background: #fff;
+  transform: translateX(${(props) => (props.$isOpen ? "0" : "100%")});
+  transition: transform 0.3s ease;
+  z-index: 20;
+`;
+
+const MenuLogo = styled.img`
+  display: flex;
+  width: 24px;
+  height: 24px;
+  padding: 5px 3px;
+  justify-content: center;
+  align-items: center;
 `;
