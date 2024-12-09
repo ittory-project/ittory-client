6f5af8cfa43c407cc0e2aee7f87b23215db849d8
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2024-12-09 16:32:45 +0900

    memberIndex 추가

--- src/components/InvitePage/Loading.tsx
+++ src/components/InvitePage/Loading.tsx
@@ -4,18 +4,50 @@ import styled from "styled-components";
 interface Props {
   loadstatus: boolean;
   setLoad: React.Dispatch<React.SetStateAction<boolean>>;
+  setLoadstatus: React.Dispatch<React.SetStateAction<boolean>>;
+  memberIndex: number;
 }
 
-export const Loading = ({ loadstatus, setLoad }: Props) => {
+export const Loading = ({
+  loadstatus,
+  setLoad,
+  setLoadstatus,
+  memberIndex,
+}: Props) => {
   useEffect(() => {
     console.log("로딩중");
     if (loadstatus === true) {
+      setLoad(true);
       console.log(loadstatus);
     } else {
       setLoad(false);
       console.log(loadstatus);
     }
-  }, [loadstatus]);
+  }, [loadstatus, setLoadstatus]);
+
+  useEffect(() => {
+    console.log("로딩중");
+    if (loadstatus === true) {
+      setLoad(true);
+      console.log(loadstatus);
+    } else {
+      if (memberIndex > -1) {
+        setLoad(false);
+        console.log(loadstatus);
+      }
+    }
+  }, [memberIndex]);
+
+  useEffect(() => {
+    console.log("로딩중");
+    if (loadstatus === true) {
+      setLoad(true);
+      console.log(loadstatus);
+    } else {
+      setLoad(false);
+      console.log(loadstatus);
+    }
+  }, []);
 
   return <div></div>;
 };
