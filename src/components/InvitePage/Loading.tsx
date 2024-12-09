e586f4273aace82579e631b3d85b6ffbf330bc19
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2024-12-09 12:56:21 +0900

    인자 전달로 변경

--- src/components/InvitePage/Loading.tsx
+++ src/components/InvitePage/Loading.tsx
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-export const Loading = () => {
+interface Props {
+  loadstatus: boolean;
+  setLoad: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const Loading = ({ loadstatus, setLoad }: Props) => {
+  useEffect(() => {
+    console.log("로딩중");
+    if (loadstatus === true) {
+      console.log(loadstatus);
+    } else {
+      console.log(loadstatus);
+      setLoad(false);
+    }
+  }, [loadstatus]);
   return <div>로딩중</div>;
 };
