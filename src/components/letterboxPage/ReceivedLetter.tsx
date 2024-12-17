d55a758af7b2ff3a77337b79dbdb14c39c212ccf
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2024-12-17 10:53:32 +0900

    받은편지함 요일 추가

--- src/components/letterboxPage/ReceivedLetter.tsx
+++ src/components/letterboxPage/ReceivedLetter.tsx
@@ -78,8 +78,11 @@ export const ReceivedLetter = ({
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
 
+    const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
+    const weekday = weekdays[date.getDay()];
+
     return (
-      <StyledDeliverDay>{`${year}. ${month}. ${day} 전달`}</StyledDeliverDay>
+      <StyledDeliverDay>{`${year}. ${month}. ${day} (${weekday}) 전달`}</StyledDeliverDay>
     );
   };
 
