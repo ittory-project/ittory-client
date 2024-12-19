fe2df45273f954c8c43e67b7011a56e2b7b959c1
Dahyeon <73732743+jrary@users.noreply.github.com>
2024-12-20 00:19:27 +0900

    다양한 오류 (#136)

--- src/pages/receive/ReceiveLetterPage.tsx
+++ src/pages/receive/ReceiveLetterPage.tsx
@@ -5,11 +5,11 @@ export const ReceiveLetterPage = () => {
   return (
     <div>
       <Helmet>
-        <title>편지 받기임</title>
+        {/* <title>편지 받기임</title>
         <meta property="og:title" content="잇토리편지받기" />
         <meta property="og:description" content="편받페이지" />
         <meta property="og:image" content="../public/img/main_logo.svg" />
-        <meta property="og:url" content={`${import.meta.env.VITE_FRONT_URL}/receive/:letterId`} />
+        <meta property="og:url" content={`${import.meta.env.VITE_FRONT_URL}/receive/:letterId`} /> */}
       </Helmet>
       <ReceiveLetter />
     </div>
