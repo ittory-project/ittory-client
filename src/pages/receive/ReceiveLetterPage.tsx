114d08c3f01e9b7fe87d9623d05ebaf51e8eb332
jrary <jrary@naver.com>
2024-12-22 20:51:24 +0900

    뒤로가기 설정이 너무 안됨

--- src/pages/receive/ReceiveLetterPage.tsx
+++ src/pages/receive/ReceiveLetterPage.tsx
@@ -1,16 +1,8 @@
-import { Helmet } from "react-helmet";
 import { ReceiveLetter } from "../../components/receivePage/ReceiveLetter";
 
 export const ReceiveLetterPage = () => {
   return (
     <div>
-      <Helmet>
-        {/* <title>편지 받기임</title>
-        <meta property="og:title" content="잇토리편지받기" />
-        <meta property="og:description" content="편받페이지" />
-        <meta property="og:image" content="../public/img/main_logo.svg" />
-        <meta property="og:url" content={`${import.meta.env.VITE_FRONT_URL}/receive/:letterId`} /> */}
-      </Helmet>
       <ReceiveLetter />
     </div>
   );
