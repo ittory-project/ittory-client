a454bbc4bda6b23d8a52257a75ca8fbfb5bdaba1
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2024-12-23 05:52:43 +0900

    뒤로가기 막기 코드 추가

--- src/pages/create/CreatePage.tsx
+++ src/pages/create/CreatePage.tsx
@@ -1,6 +1,21 @@
 import { Create } from "../../components/createPage/Create";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-export const CreatePage = () => {  
+export const CreatePage = () => {
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate("/");
+  };
+  useEffect(() => {
+    history.pushState(null, "", window.location.href);
+
+    window.addEventListener("popstate", handleGoBack);
+
+    return () => {
+      window.removeEventListener("popstate", handleGoBack);
+    };
+  }, []);
   return (
     <div>
       <Create />
