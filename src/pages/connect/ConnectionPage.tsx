e1d7876366ed9bc041d4a5b0db979aa79630f280
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2024-12-23 05:52:43 +0900

    뒤로가기 막기 코드 추가

--- src/pages/connect/ConnectionPage.tsx
+++ src/pages/connect/ConnectionPage.tsx
@@ -1,6 +1,21 @@
 import { Connection } from "../../components/connectionPage/Connection";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export const ConnectionPage = () => {
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate("/");
+  };
+  useEffect(() => {
+    history.pushState(null, "", window.location.href);
+
+    window.addEventListener("popstate", handleGoBack);
+
+    return () => {
+      window.removeEventListener("popstate", handleGoBack);
+    };
+  }, []);
   return (
     <div>
       <Connection />
