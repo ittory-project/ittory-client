d266b4624cdda0cc48ddffbe36ed543a46921570
jrary <jrary@naver.com>
2025-01-03 22:45:04 +0900

    모바일 화면 트리거 재설정

--- src/components/createPage/CoverDeco/CoverStyle.tsx
+++ src/components/createPage/CoverDeco/CoverStyle.tsx
@@ -163,7 +163,7 @@ export default function CoverStyle({
 
       if (inputRef.current && inputRef.current.contains(e.target as Node)) {
         if (keyboardHeight > 0) {
-          if (window.innerWidth < 431) {
+          if (window.innerWidth < 850) {
             setIsKeyboardOpen(true);
             setIsKeyboardOpen(true);
             setKeyboardHeight(heightDiff);
--- src/components/sharePage/ShareLetter.tsx
+++ src/components/sharePage/ShareLetter.tsx
@@ -39,10 +39,10 @@ export const ShareLetter = () => {
   }, [query]);
 
   const handleResize = () => {
-    window.innerWidth < 431 ? setIsMobile(true) : setIsMobile(false)
+    window.innerWidth < 850 ? setIsMobile(true) : setIsMobile(false)
   }
   useEffect(() => {
-    window.innerWidth < 431 ? setIsMobile(true) : setIsMobile(false)
+    window.innerWidth < 850 ? setIsMobile(true) : setIsMobile(false)
     window.addEventListener('resize', handleResize)
     return () => {
       window.removeEventListener('resize', handleResize)
