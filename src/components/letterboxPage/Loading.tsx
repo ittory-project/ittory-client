d4f66ac500dbcf9d564a76c7fb3111b827a1069c
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2025-01-06 17:37:34 +0900

    편지함 기능 QA 반영

---
+++ src/components/letterboxPage/Loading.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import animation from "../../../public/assets/loading.json";
+import Player from "lottie-react";
+import styled from "styled-components";
+
+export const Loading = () => {
+  return (
+    <BackGround>
+      <Player
+        animationData={animation}
+        loop
+        autoplay
+        style={{
+          height: "112px",
+          width: "200px",
+          margin: "0 auto",
+        }}
+      />
+    </BackGround>
+  );
+};
+
+const BackGround = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: calc(var(--vh, 1vh) * 100);
+  width: 100%;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  justify-content: center;
+  background: #f1f3f5;
+`;
