0d75e49dec7ed1ccbbad71c70379c2181715b46a
Seongbin Kim <seongbin9786@gmail.com>
2025-03-20 12:55:26 +0900

    fix: 디자인 QA 반영 작업 완료 (대부분)

--- src/components/loginPage/Login.tsx
+++ src/components/loginPage/Login.tsx
@@ -21,59 +21,91 @@ export const Login = () => {
 
   return (
     <LoginContainer>
-      <CloseBtn onClick={handleCloseBtn} src="/assets/btn_close.svg" />
-      <Logo src={MainLogo} alt="Logo" />
-      <Desc>마음을 표현하는 새로운 방법!</Desc>
-      <LoginBtn onClick={kakaoLogin}>
-        <Icon src="/assets/kakao_logo.png" alt="Login Icon" />
-        카카오로 시작하기
-      </LoginBtn>
-      <LoginDesc>
-        로그인 하시면{" "}
-        <a href={개인정보처리방침URL} target="_blank" rel="noopener noreferrer">
-          개인정보처리방침
-        </a>
-        과{" "}
-        <a href={서비스이용약관URL} target="_blank" rel="noopener noreferrer">
-          서비스이용약관
-        </a>{" "}
-        에 동의하게 됩니다.
-      </LoginDesc>
+      <CloseBtn
+        onClick={handleCloseBtn}
+        src="/assets/btn_close.svg"
+        role="button"
+      />
+      <LogoArea>
+        <LogoImage src={MainLogo} alt="Logo" />
+        <LogoDescription>마음을 표현하는 새로운 방법!</LogoDescription>
+      </LogoArea>
+      <BottomArea>
+        <LoginBtn onClick={kakaoLogin}>
+          <Icon src="/assets/kakao_logo.png" alt="Login Icon" />
+          카카오로 시작하기
+        </LoginBtn>
+        <LoginDesc>
+          로그인 하시면{" "}
+          <a
+            href={개인정보처리방침URL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            개인정보처리방침
+          </a>
+          과{" "}
+          <a href={서비스이용약관URL} target="_blank" rel="noopener noreferrer">
+            서비스이용약관
+          </a>
+          에 동의하게 됩니다.
+        </LoginDesc>
+      </BottomArea>
     </LoginContainer>
   );
 };
 
 const LoginContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
   height: 100%;
-  padding: 12px 10px;
-  box-sizing: border-box;
   background-color: white;
   position: relative;
+
+  font-family: var(--Typography-family-title);
 `;
 
+// TODO: svg icon 사용
+// TODO: stylelint 사용? (스타일 속성 간 정렬)
 const CloseBtn = styled.img`
   height: 20px;
   width: 20px;
+
   position: absolute;
-  top: 20px;
-  right: 20px;
+  top: 12px;
+  right: 16px;
 `;
 
-const Logo = styled.img`
+const LogoArea = styled.div`
+  position: absolute;
+  top: 128px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  align-items: center;
+`;
+
+const LogoImage = styled.img`
   width: 236px;
   height: 90px;
-  margin: 50px 0px 16px;
 `;
 
-const Desc = styled.div`
+const LogoDescription = styled.div`
   font-size: 14px;
-  color: #868e96;
-  margin-bottom: 200px;
+  font-weight: 400;
+  color: var(--Color-grayscale-gray600);
+`;
+
+const BottomArea = styled.div`
+  position: absolute;
+  bottom: 40px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
 `;
 
 const Icon = styled.img`
@@ -82,35 +114,42 @@ const Icon = styled.img`
   margin-right: 8px;
 `;
 
+// TODO: button 컴포넌트 공통화
 const LoginBtn = styled.button`
-  width: 288px;
-  max-width: 400px;
   height: 48px;
+  width: 100%;
+  max-width: 288px;
+
   background-color: #fee500;
+
+  font-weight: bold;
+  font-size: 16px;
   color: black;
+
   border: none;
   border-radius: 2rem;
   padding: 0 20px;
-  cursor: pointer;
-  font-size: 16px;
-  margin: 0 auto 16px;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
 const LoginDesc = styled.div`
+  width: 100%;
+  max-width: 288px;
+
   font-size: 12px;
-  color: #868E96;
-  max-width: 300px;
-  margin-top: 10px;
+  color: var(--Color-grayscale-gray600);
 
   a {
-    color: #868E96;
+    color: var(--Color-grayscale-gray600);
+
     text-decoration: underline;
     cursor: pointer;
-  
+
+    // Q. 로그인 페이지 디자인에 hover 색상이 없던 것 같은데 정의된 위치(Figma URL)?
     &:hover {
-    color: #495057;
+      color: #495057;
+    }
   }
 `;
