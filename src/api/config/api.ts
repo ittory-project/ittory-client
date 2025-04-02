4cb350549e762fa5c83490d64c0d6f56c6ec191e
Seongbin Kim <seongbin9786@gmail.com>
2025-04-02 12:33:36 +0900

    Refactor: 토큰 리프레시 로직 분리

--- src/api/config/api.ts
+++ src/api/config/api.ts
@@ -1,8 +1,6 @@
-import axios, { AxiosError, AxiosResponse, HttpStatusCode } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { getJwt } from './setToken';
-import { API_ERROR_CODE } from './constants';
-import { refreshAccessToken } from './tokenRefresh';
-import { forceLogout } from './logout';
+import { awaitTokenRefresh, tryTokenRefrsh } from './tokenRefresh';
 
 export interface BaseResponse<T = unknown> {
   success: boolean;
@@ -29,41 +27,11 @@ const apiSetting = {
   },
 };
 
-// TODO: Sentry 등으로 오류 모니터링
-const errorHandler = async (
-  error: AxiosError<BaseResponse<unknown>> | Error,
-) => {
-  if (!axios.isAxiosError(error)) {
-    console.error(`AxiosError가 아닌 네트워크 오류: ${error}`);
-    throw error;
-  }
-
-  console.error('네트워크 오류:', error);
-  if (
-    error.response?.status === HttpStatusCode.Unauthorized &&
-    error.response?.data?.code === API_ERROR_CODE.INVALID_ACCESS_TOKEN &&
-    error.config
-  ) {
-    try {
-      refreshAccessToken(error.config);
-      // NOTE: axios는 axios.post와 같은 요청 함수이다.
-      // originalRequest는 단순한 설정 객체이므로, 반환하면 요청이 발생하지 않고, 그냥 설정 객체가 호출 측에 넘겨진다.
-      return axios(error.config);
-    } catch (refreshError) {
-      console.error('토큰 갱신 시도 실패:', refreshError);
-      forceLogout();
-    }
-  }
-
-  // NOTE: error 객체를 반환하면 Promise.resolve()로 감싸져 성공 응답으로 처리되므로,
-  // 명시적으로 Promise.reject()로 반환해야 한다.
-  return Promise.reject(error);
-};
-
 export const api = axios.create(apiSetting);
 
 // localStorage에서 Access Token 불러옴
 // TODO: localStorage 방식 제거 시 함께 제거
 api.defaults.headers.common['Authorization'] = `Bearer ${getJwt()}`;
 
-api.interceptors.response.use(null, errorHandler);
+api.interceptors.request.use(awaitTokenRefresh);
+api.interceptors.response.use(null, tryTokenRefrsh);
--- src/api/config/tokenRefresh.ts
+++ src/api/config/tokenRefresh.ts
@@ -1,12 +1,26 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, {
+  AxiosError,
+  AxiosRequestConfig,
+  HttpStatusCode,
+  InternalAxiosRequestConfig,
+} from 'axios';
 import { setJwt } from './setToken';
+import { api, BaseResponse } from './api';
+import { forceLogout } from './logout';
 
-const refreshEndpoint = `${import.meta.env.VITE_SERVER_URL}/auth/refresh`;
+// TODO: /api prefix를 axios 인스턴스에 공통화
+const refreshEndpoint = `${import.meta.env.VITE_SERVER_URL}/api/auth/refresh`;
 
 export const GetNewAccessToken = async (
   config: AxiosRequestConfig,
 ): Promise<string> => {
-  const response = await axios.post(refreshEndpoint, {}, config);
+  const response = await api.post(
+    refreshEndpoint,
+    {
+      accessToken: (config.headers?.Authorization ?? '')?.split(' ')[1],
+    },
+    config,
+  );
 
   // TODO: 오류 모니터링
   if (!response.data.success) {
@@ -17,11 +31,68 @@ export const GetNewAccessToken = async (
 };
 
 export const refreshAccessToken = async (
-  originalRequest: AxiosRequestConfig,
+  originalRequest: AxiosRequestConfig | InternalAxiosRequestConfig,
 ) => {
   const newToken = await GetNewAccessToken(originalRequest);
+  const newAuthorization = `Bearer ${newToken}`;
 
   // TODO: localStorage 방식 제거 시 함께 제거
   setJwt(newToken);
-  axios.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
+  api.defaults.headers.common['Authorization'] = newAuthorization;
+
+  return newAuthorization;
+};
+
+// 동시 실행되는 요청들에 대해 토큰 갱신 요청을 공유하기 위함
+let refreshRequest: Promise<string> | null = null;
+
+export const awaitTokenRefresh = async (config: InternalAxiosRequestConfig) => {
+  if (refreshRequest && config.headers) {
+    const newAuthorization = await refreshRequest;
+    config.headers.Authorization = newAuthorization;
+  }
+  return config;
+};
+
+// TODO: Sentry 등으로 오류 모니터링
+export const tryTokenRefrsh = async (
+  error: AxiosError<BaseResponse<unknown>> | Error,
+) => {
+  // Q. throw로 처리하면 더 깔끔할 것 같은데 조사 필요
+  if (!axios.isAxiosError(error)) {
+    console.error(`AxiosError가 아닌 네트워크 오류: ${error}`);
+    return Promise.reject(error);
+  }
+
+  const config = error?.config;
+  if (!config) {
+    console.error(`Axios Config 객체가 없는 네트워크 오류: ${error}`);
+    return Promise.reject(error);
+  }
+
+  console.error('네트워크 오류:', error);
+
+  if (error.response?.status === HttpStatusCode.Unauthorized) {
+    if (!refreshRequest) {
+      refreshRequest = new Promise<string>((resolve, reject) => {
+        refreshAccessToken(config)
+          .then((token) => {
+            resolve(token);
+          })
+          .catch((refreshError) => {
+            console.error('토큰 갱신 시도 실패:', refreshError);
+            forceLogout();
+            reject(refreshError);
+          });
+      });
+    }
+
+    const newAuthorization = await refreshRequest;
+    config.headers.Authorization = newAuthorization;
+    return api(config);
+  }
+
+  // NOTE: error 객체를 반환하면 Promise.resolve()로 감싸져 성공 응답으로 처리되므로,
+  // 명시적으로 Promise.reject()로 반환해야 한다.
+  return Promise.reject(error);
 };
