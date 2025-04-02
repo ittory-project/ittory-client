4ba3f4466edaa73f56d75a56578162abb2f18354
Seongbin Kim <seongbin9786@gmail.com>
2025-04-03 08:14:10 +0900

    Refactor: LoginRedirect에서 로그인 로직 분리하고 오류 처리 추가

--- src/components/loginPage/LoginRedirect.tsx
+++ src/components/loginPage/LoginRedirect.tsx
@@ -1,49 +1,36 @@
 import { useEffect } from 'react';
-import {
-  getKakaoToken,
-  getUserId,
-  setJwt,
-  setUserId,
-} from '../../api/config/setToken';
-import { useNavigate } from 'react-router-dom';
-import { postLogin } from '../../api/service/AuthService';
-import { api } from '../../api/config/api';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { login } from '../../api/config/login';
 
 export const LoginRedirect = () => {
-  const code = new URL(window.location.href).searchParams.get('code');
+  const location = useLocation();
+  const kakaoSocialLoginCode = new URLSearchParams(location.search).get('code');
   const navigate = useNavigate();
 
-  const login = async (code: string) => {
-    try {
-      const kakaoToken = await getKakaoToken(code);
-      if (kakaoToken) {
-        const response = await postLogin(kakaoToken.accessToken);
-        const newAuthorization = `Bearer ${response.accessToken}`;
+  useEffect(() => {
+    const handleLogin = async () => {
+      if (!kakaoSocialLoginCode) {
+        console.log('소셜 로그인 코드가 없습니다.');
+        return;
+      }
 
-        api.defaults.headers.common['Authorization'] = newAuthorization;
-        setJwt(newAuthorization);
-        setUserId(response.accessToken);
-        console.log(`유저 아이디: ${getUserId()}`);
+      try {
+        await login(kakaoSocialLoginCode);
 
-        // TODO: login 함수에서 로직 분리하기
         if (localStorage.letterId) {
           const letterId = localStorage.letterId;
-          console.log(letterId);
           navigate(`/join/${letterId}`);
         } else {
           navigate('/');
         }
+      } catch (error) {
+        // TODO: 오류 설명을 띄우기
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    };
 
-  useEffect(() => {
-    if (code) {
-      login(code);
-    }
-  }, [code]);
+    handleLogin();
+  }, [kakaoSocialLoginCode, navigate]);
 
   // TODO: Full Screen Loading 화면으로 대체
   return <div>로그인 중입니다</div>;
