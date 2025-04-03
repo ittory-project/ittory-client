92d183cea1a15e86d738ccc0bc40ffa4da2e12cd
Seongbin Kim <seongbin9786@gmail.com>
2025-04-03 18:21:36 +0900

    Fix: stompInterceptor에도 authorization 포맷 변경 반영

--- src/api/config/stompInterceptor.ts
+++ src/api/config/stompInterceptor.ts
@@ -1,11 +1,16 @@
-import { Client } from "@stomp/stompjs";
-import { getJwt } from "./setToken";
+import { Client } from '@stomp/stompjs';
+import { getJwt } from './setToken';
 
 export const stompClient = (): Client => {
+  const authorization = getJwt();
+  if (!authorization) {
+    throw new Error('Authorization is not set');
+  }
+
   const client = new Client({
     brokerURL: `${import.meta.env.VITE_SERVER_URL}/connection`,
     connectHeaders: {
-      Authorization: `Bearer ${getJwt()}`,
+      Authorization: authorization,
     },
     debug: (str) => {
       console.log(str);
@@ -13,10 +18,10 @@ export const stompClient = (): Client => {
   });
 
   client.onStompError = (frame) => {
-    console.error("STOMP error:", frame.headers.message);
+    console.error('STOMP error:', frame.headers.message);
   };
   client.onDisconnect = () => {
-    console.log("Disconnected from WebSocket");
+    console.log('Disconnected from WebSocket');
   };
 
   return client;
