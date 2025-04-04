6fd24006479bc9ddefbe4a2e82d4f07acf221f6e
Seongbin Kim <seongbin9786@gmail.com>
2025-04-04 15:10:40 +0900

    Feat: AccessTokenManager에 의존하도록 변경

--- src/api/config/tokenRefresh.ts
+++ src/api/config/tokenRefresh.ts
@@ -3,9 +3,9 @@ import axios, {
   HttpStatusCode,
   InternalAxiosRequestConfig,
 } from 'axios';
-import { setJwt } from './setToken';
 import { api, BaseResponse } from './api';
 import { forceLogout } from './logout';
+import { AccessTokenManager } from './accessTokenManager';
 
 // TODO: /api prefix를 axios 인스턴스에 공통화
 const refreshEndpoint = `${import.meta.env.VITE_SERVER_URL}/api/auth/refresh`;
@@ -21,23 +21,28 @@ export const fetchNewAccessToken = async () => {
     },
   );
 
-  const newAuthorization = `Bearer ${response.data.data.accessToken}`;
-
-  // TODO: localStorage 방식 제거 시 함께 제거
-  setJwt(newAuthorization);
-  api.defaults.headers.common['Authorization'] = newAuthorization;
-
-  return newAuthorization;
+  AccessTokenManager.getInstance().setAccessToken(
+    `Bearer ${response.data.data.accessToken}`,
+  );
 };
 
 // 동시 실행되는 요청들에 대해 토큰 갱신 요청을 공유하기 위함
-let refreshRequest: Promise<string> | null = null;
+let refreshRequest: Promise<void> | null = null;
+
+export const attachAccessToken = (config: InternalAxiosRequestConfig) => {
+  // NOTE: Refresh 요청 시에도 해당 Interceptor를 통과
+  if (!refreshRequest) {
+    config.headers.Authorization =
+      AccessTokenManager.getInstance().getAccessToken();
+  }
+  return config;
+};
 
 export const awaitTokenRefresh = async (config: InternalAxiosRequestConfig) => {
   if (refreshRequest && config.headers && config.url !== refreshEndpoint) {
     try {
-      const newAuthorization = await refreshRequest;
-      config.headers.Authorization = newAuthorization;
+      await refreshRequest;
+      attachAccessToken(config);
     } catch (refreshError) {
       return Promise.reject(refreshError);
     }
@@ -61,25 +66,26 @@ export const tryTokenRefrsh = async (
     return Promise.reject(error);
   }
 
-  console.error('네트워크 오류:', error);
-
   if (error.response?.status === HttpStatusCode.Unauthorized) {
     if (!refreshRequest) {
       refreshRequest = (async () => {
         try {
-          return await fetchNewAccessToken();
+          await fetchNewAccessToken();
+          attachAccessToken(config);
         } catch (refreshError) {
           console.error('토큰 갱신 시도 실패:', refreshError);
           forceLogout();
           throw refreshError;
+        } finally {
+          refreshRequest = null;
         }
       })();
     }
 
-    const newAuthorization = await refreshRequest;
-    config.headers.Authorization = newAuthorization;
     return api(config);
   }
+
+  console.error('처리하지 못한 네트워크 오류:', error);
 
   // NOTE: error 객체를 반환하면 Promise.resolve()로 감싸져 성공 응답으로 처리되므로,
   // 명시적으로 Promise.reject()로 반환해야 한다.
