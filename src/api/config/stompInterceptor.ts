08bc54e6755f14deb6c054c9c95f090eed235a3e
Seongbin Kim <seongbin9786@gmail.com>
2025-04-04 15:10:40 +0900

    Feat: AccessTokenManager에 의존하도록 변경

--- src/api/config/stompInterceptor.ts
+++ src/api/config/stompInterceptor.ts
@@ -1,10 +1,12 @@
 import { Client } from '@stomp/stompjs';
-import { getJwt } from './setToken';
+import { fetchNewAccessToken } from './tokenRefresh';
+import { forceLogout } from './logout';
+import { AccessTokenManager } from './accessTokenManager';
 
 export const stompClient = (): Client => {
-  const authorization = getJwt();
+  const authorization = AccessTokenManager.getInstance().getAccessToken();
   if (!authorization) {
-    throw new Error('Authorization is not set');
+    throw new Error('WebSocket 인증에 필요한 엑세스 토큰이 없습니다.');
   }
 
   const client = new Client({
@@ -15,11 +17,32 @@ export const stompClient = (): Client => {
     debug: (str) => {
       console.log(str);
     },
+    // 기본 값을 명시적으로 사용
+    reconnectDelay: 5000,
+    heartbeatIncoming: 4000,
+    heartbeatOutgoing: 4000,
   });
 
-  client.onStompError = (frame) => {
-    console.error('STOMP error:', frame.headers.message);
+  // FIXME: 토큰 리프레시가 안 되는 경우가 있는데 정확한 원인 분석이 어려워 확인 필요
+  client.onStompError = async (frame) => {
+    // 인증 관련 오류인 경우 토큰 갱신 시도
+    if (frame.headers.message?.toLowerCase().includes('unauthorized')) {
+      try {
+        await fetchNewAccessToken();
+
+        if (client.connected) {
+          client.deactivate();
+        }
+        client.activate();
+      } catch (error) {
+        console.error('토큰 리프레시에 실패했습니다:', error);
+        forceLogout();
+      }
+    } else {
+      console.error('STOMP error:', frame.headers.message);
+    }
   };
+
   client.onDisconnect = () => {
     console.log('Disconnected from WebSocket');
   };
