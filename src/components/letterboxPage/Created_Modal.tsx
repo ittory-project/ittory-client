f9a388ba01db82d080bdbe5991d04de5bf16d5ec
Seongbin Kim <seongbin9786@gmail.com>
2025-04-22 21:52:41 +0900

    Refactor: 신규 로거 도입에 따른 기존 console.log/error 마이그레이션 (3)

--- src/components/letterboxPage/Created_Modal.tsx
+++ src/components/letterboxPage/Created_Modal.tsx
@@ -100,7 +100,7 @@ export const Created_Modal = ({
             setCopied(true);
             setTimeout(() => setCopied(false), 3000);
           } catch (error) {
-            console.error('공유 실패: ', error);
+            logger.error('공유 실패: ', error);
             fallbackCopyTextToClipboard(shareTextPc);
           }
         } else {
