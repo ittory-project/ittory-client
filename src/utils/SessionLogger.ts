775b71ab760a7711a32f576e8075fcb366924888
Seongbin Kim <seongbin9786@gmail.com>
2025-04-22 18:22:47 +0900

    Feat: Session Logger 구현체 추가

---
+++ src/utils/SessionLogger.ts
@@ -0,0 +1,116 @@
+type LogLevel = 'info' | 'warn' | 'error' | 'debug';
+type Feature = 'nav' | 'api' | 'error';
+
+/**
+ * Session(Tab) 단위로 로그를 출력하고 보관하는 Logger
+ * - 새로 고침 시에도 로그 유지
+ * - 로그 초기화를 위해서는 신규 탭 사용 필요
+ */
+export class SessionLogger {
+  private feature: Feature;
+  private logs: string[] = [];
+  private static enabledFeatures: Feature[] = [];
+  private static enabledLogLevels: LogLevel[] = [];
+
+  constructor(feature: Feature) {
+    this.feature = feature;
+    this.loadLogs();
+  }
+
+  static {
+    // eslint-disable-next-line
+    // @ts-ignore
+    window.exportSessionLogs = SessionLogger.exportAsTxt;
+  }
+
+  static enableFeatures(features: Feature[]) {
+    this.enabledFeatures.push(...features);
+  }
+
+  static enableLogLevels(logLevels: LogLevel[]) {
+    this.enabledLogLevels.push(...logLevels);
+  }
+
+  static exportAsTxt(): void {
+    const rawData = JSON.parse(
+      sessionStorage.getItem('logs') ?? '[]',
+    ) as string[];
+    const content = rawData.join('\r\n');
+
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `ittory_session_log_${new Date().toISOString()}.txt`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  }
+
+  info(...args: unknown[]) {
+    this.log('info', ...args);
+  }
+
+  warn(...args: unknown[]) {
+    this.log('warn', ...args);
+  }
+
+  error(...args: unknown[]) {
+    this.log('error', ...args);
+  }
+
+  debug(...args: unknown[]) {
+    this.log('debug', ...args);
+  }
+
+  private loadLogs() {
+    const storedLogs = sessionStorage.getItem('logs');
+    if (storedLogs) {
+      this.logs = JSON.parse(storedLogs);
+    }
+  }
+
+  private appendLog(log: string) {
+    this.logs.push(log);
+    sessionStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  private formatContent(args: unknown[]): string {
+    return args
+      .map((arg) => {
+        if (typeof arg === 'object') {
+          return JSON.stringify(arg);
+        }
+        return String(arg);
+      })
+      .join(' ');
+  }
+
+  private isLogEnabled(type: LogLevel): boolean {
+    return (
+      SessionLogger.enabledLogLevels.includes(type) &&
+      SessionLogger.enabledFeatures.includes(this.feature)
+    );
+  }
+
+  private log(type: LogLevel, ...args: unknown[]) {
+    if (!this.isLogEnabled(type)) {
+      return;
+    }
+
+    const time = new Date().toLocaleTimeString('ko-KR', {
+      hour12: false,
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+    const content = this.formatContent(args);
+    const logEntry = `[${time}][${type}][${this.feature}] ${content}`;
+
+    this.appendLog(logEntry);
+
+    // 콘솔에 출력
+    console[type](`[${type}][${this.feature}]`, ...args);
+  }
+}
