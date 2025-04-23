d66f7af32dc726eb9004ac7ae6fa1ad0d18a1720
Seongbin Kim <seongbin9786@gmail.com>
2025-04-23 12:57:12 +0900

    Chore: ESLint, Prettier 기반의 unused imports 제거, import 정렬 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 // NOTE: tseslint.config를 [] 에 감싸서 내보내면 CLI가 제대로 동작하지 않음
 export default tseslint.config({
@@ -23,6 +24,7 @@ export default tseslint.config({
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
     react,
+    'unused-imports': unusedImports,
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
@@ -31,6 +33,16 @@ export default tseslint.config({
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
     ],
     'no-multiple-empty-lines': 'error',
     'no-unused-vars': 'error',
