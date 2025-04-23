6c6beb6115a33e9d1ff19c2ed780a9550197d7d2
Seongbin Kim <seongbin9786@gmail.com>
2025-04-23 13:31:46 +0900

    Chore: style property 그룹 간 공백 추가

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,14 @@
+import propertyGroups from 'stylelint-config-recess-order/groups';
+
 /** @type {import('stylelint').Config} */
 export default {
+  rules: {
+    'order/properties-order': propertyGroups.map((group) => ({
+      ...group,
+      emptyLineBefore: 'always',
+      noEmptyLineBetween: true,
+    })),
+  },
   overrides: [
     {
       files: ['**/*.{js,jsx,ts,tsx}'],
