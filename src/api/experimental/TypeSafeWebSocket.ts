f4147e05dff6cb1b5330b2fe883088eb120e2032
Seongbin Kim <seongbin9786@gmail.com>
2025-05-04 16:17:53 +0900

    Feat: RL1M-304 웹소켓 페이로드 타입 추론 가능하게 - subscribe 연동을 위해 함수 한 단계 더 추가

--- src/api/experimental/TypeSafeWebSocket.ts
+++ src/api/experimental/TypeSafeWebSocket.ts
@@ -1,10 +1,14 @@
+import { Client } from '@stomp/stompjs';
+
+import { createStompClient } from './createStompClient';
+
 type ResponseHandler<Payload> = (_payload: Payload) => void;
 
 type ResponseHandlers = Record<string, ResponseHandler<unknown>>;
 
 type ChannelConfig<Handlers extends ResponseHandlers> = (
   _handlers: Handlers,
-) => void;
+) => (_payload: unknown) => void;
 
 // 전체 Definition 타입
 export type ResponseMapperDefinition = Record<
@@ -16,17 +20,22 @@ export class TypeSafeWebSocket<
   UserResponseMapperDefinition extends ResponseMapperDefinition,
 > {
   private definition: UserResponseMapperDefinition;
+  private stompClient: Client;
 
-  constructor(definition: UserResponseMapperDefinition) {
-    this.definition = definition;
+  constructor(responseMapperDefinition: UserResponseMapperDefinition) {
+    this.definition = responseMapperDefinition;
+    this.stompClient = createStompClient();
   }
 
   // unsubscribe만 반환하면 됨!
-  public subscribe<Channel extends keyof UserResponseMapperDefinition>(
+  // keyof를 쓰면 아무리 string key로 설정했더라도 항상 number|string|symbol 타입으로 추론되므로 & string을 추가
+  public subscribe<Channel extends keyof UserResponseMapperDefinition & string>(
     channel: Channel,
     handlerConfig: Parameters<UserResponseMapperDefinition[Channel]>[0],
   ) {
     //do nothing!
-    console.log(this.definition[channel](handlerConfig));
+    this.stompClient.subscribe(channel, (message) => {
+      this.definition[channel](handlerConfig)(message);
+    });
   }
 }
--- src/api/experimental/instance.ts
+++ src/api/experimental/instance.ts
@@ -7,19 +7,22 @@ import {
 // Mapper + Switch-Case 구현 파트
 // TODO: 사용자가 (채널, 이벤트, 페이로드 타입)을 먼저 정의해놓고 제네릭으로 전달하면 자동 완성이 좀 될텐데 지금은 아예 안 되긴 함
 export const userResponseMapperDefinition: ResponseMapperDefinition = {
-  letter: (handlers: {
-    exit?: (_payload: string) => void;
-    submit?: (_payload: number) => void;
-    timeout?: (_payload: boolean) => void;
-  }) => {
-    if (Math.random() > 0.5) {
-      handlers.exit?.('exit');
-    } else if (Math.random() > 0.3) {
-      handlers.submit?.(1);
-    } else {
-      handlers.timeout?.(true);
-    }
-  },
+  letter:
+    (handlers: {
+      exit?: (_payload: string) => void;
+      submit?: (_payload: number) => void;
+      timeout?: (_payload: boolean) => void;
+    }) =>
+    (payload: unknown) => {
+      const _processed = JSON.parse(payload as string);
+      if (Math.random() > 0.5) {
+        handlers.exit?.('exit');
+      } else if (Math.random() > 0.3) {
+        handlers.submit?.(1);
+      } else {
+        handlers.timeout?.(true);
+      }
+    },
 };
 
 export const websocketApi = new TypeSafeWebSocket(userResponseMapperDefinition);
