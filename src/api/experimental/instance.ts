64b2d1fa1755ce52f34da4debf782cc0a77c1d9e
Seongbin Kim <seongbin9786@gmail.com>
2025-05-04 16:07:44 +0900

    Feat: RL1M-304 웹소켓 페이로드 타입 추론 가능하게 - 설명 추가

--- src/api/experimental/instance.ts
+++ src/api/experimental/instance.ts
@@ -1,21 +1,25 @@
-import { TypeSafeWebSocket } from './TypeSafeWebSocket';
+import {
+  ResponseMapperDefinition,
+  TypeSafeWebSocket,
+} from './TypeSafeWebSocket';
 
 // channel로 전달되는 raw response를 각 response로 변환하는 과정은 사용자가 직접 정의해야 함
-export const userDefinition = {
-  letter: (responseTypes: {
+// Mapper + Switch-Case 구현 파트
+// TODO: 사용자가 (채널, 이벤트, 페이로드 타입)을 먼저 정의해놓고 제네릭으로 전달하면 자동 완성이 좀 될텐데 지금은 아예 안 되긴 함
+export const userResponseMapperDefinition: ResponseMapperDefinition = {
+  letter: (handlers: {
     exit?: (_payload: string) => void;
     submit?: (_payload: number) => void;
     timeout?: (_payload: boolean) => void;
   }) => {
-    // todo
     if (Math.random() > 0.5) {
-      responseTypes.exit?.('exit');
+      handlers.exit?.('exit');
     } else if (Math.random() > 0.3) {
-      responseTypes.submit?.(1);
+      handlers.submit?.(1);
     } else {
-      responseTypes.timeout?.(true);
+      handlers.timeout?.(true);
     }
   },
 };
 
-export const websocketApi = new TypeSafeWebSocket(userDefinition);
+export const websocketApi = new TypeSafeWebSocket(userResponseMapperDefinition);
