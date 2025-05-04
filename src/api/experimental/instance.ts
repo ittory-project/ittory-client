f445735450a4b858a6fed46fdfe43e2cdb3307aa
Seongbin Kim <seongbin9786@gmail.com>
2025-05-04 15:57:18 +0900

    Feat: RL1M-304 웹소켓 페이로드 타입 추론 가능하게 - definition 정의 방식 변경을 통해 response type  분기 구현할 수 있게

--- src/api/experimental/instance.ts
+++ src/api/experimental/instance.ts
@@ -1,14 +1,20 @@
 import { TypeSafeWebSocket } from './TypeSafeWebSocket';
 
+// channel로 전달되는 raw response를 각 response로 변환하는 과정은 사용자가 직접 정의해야 함
 export const userDefinition = {
-  letter: {
-    exit: (payload: unknown) => payload as string,
-    submit: (payload: unknown) => payload as string,
-    timeout: (payload: unknown) => payload as string,
-  },
-  chat: {
-    message: (payload: unknown) => payload as { text: string },
-    join: (payload: unknown) => payload as { userId: string },
+  letter: (responseTypes: {
+    exit?: (_payload: string) => void;
+    submit?: (_payload: number) => void;
+    timeout?: (_payload: boolean) => void;
+  }) => {
+    // todo
+    if (Math.random() > 0.5) {
+      responseTypes.exit?.('exit');
+    } else if (Math.random() > 0.3) {
+      responseTypes.submit?.(1);
+    } else {
+      responseTypes.timeout?.(true);
+    }
   },
 };
 
