c056d7ca915dbd5cfb209787ea6327c84807b4d9
Seongbin Kim <seongbin9786@gmail.com>
2025-05-04 16:39:15 +0900

    Feat: RL1M-304 웹소켓 페이로드 타입 추론 가능하게 - subscribe 공유 구현

--- src/api/experimental/TypeSafeWebSocket.ts
+++ src/api/experimental/TypeSafeWebSocket.ts
@@ -1,4 +1,4 @@
-import { Client } from '@stomp/stompjs';
+import { Client, StompSubscription } from '@stomp/stompjs';
 
 import { createStompClient } from './createStompClient';
 
@@ -21,21 +21,61 @@ export class TypeSafeWebSocket<
 > {
   private definition: UserResponseMapperDefinition;
   private stompClient: Client;
+  private subscriptions: Map<
+    keyof UserResponseMapperDefinition,
+    StompSubscription
+  >;
+  private channelListeners: Map<
+    keyof UserResponseMapperDefinition,
+    ((_message: unknown) => void)[]
+  >;
 
   constructor(responseMapperDefinition: UserResponseMapperDefinition) {
     this.definition = responseMapperDefinition;
     this.stompClient = createStompClient();
+    this.subscriptions = new Map();
+    this.channelListeners = new Map();
   }
 
-  // unsubscribe만 반환하면 됨!
   // keyof를 쓰면 아무리 string key로 설정했더라도 항상 number|string|symbol 타입으로 추론되므로 & string을 추가
   public subscribe<Channel extends keyof UserResponseMapperDefinition & string>(
     channel: Channel,
     handlerConfig: Parameters<UserResponseMapperDefinition[Channel]>[0],
   ) {
-    //do nothing!
-    this.stompClient.subscribe(channel, (message) => {
-      this.definition[channel](handlerConfig)(message);
-    });
+    // 아직 리스너 배열이 없으면 리스너 배열 생성
+    const channelListeners = this.channelListeners.get(channel) ?? [];
+    if (!channelListeners) {
+      this.channelListeners.set(channel, channelListeners);
+    }
+
+    // 리스너 배열에 리스너 추가
+    const currentListener = this.definition[channel](handlerConfig).bind(
+      this.definition[channel],
+    );
+    channelListeners.push(currentListener);
+
+    // 아직 구독 중이지 않으면 구독 필요
+    if (!this.subscriptions.has(channel)) {
+      const callListeners = (message: unknown) => {
+        this.channelListeners
+          .get(channel)
+          ?.forEach((listener) => listener(message));
+      };
+      const subscription = this.stompClient.subscribe(channel, callListeners);
+      this.subscriptions.set(channel, subscription);
+    }
+
+    // 모든 listeners가 없으면 구독도 종료
+    return () => {
+      const channelListeners = this.channelListeners.get(channel);
+      if (!channelListeners) {
+        return;
+      }
+      channelListeners.filter((listener) => listener !== currentListener);
+      if (channelListeners.length === 0) {
+        this.subscriptions.get(channel)?.unsubscribe();
+        this.subscriptions.delete(channel);
+      }
+    };
   }
 }
