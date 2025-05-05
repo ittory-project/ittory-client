21a5f2776f67164b1371b19762156602e5e2050f
Seongbin Kim <seongbin9786@gmail.com>
2025-05-06 08:01:04 +0900

    Feat: RL1M-304 기존 request schema 작성

--- src/api/experimental/ittoryWebSocketSchema.ts
+++ src/api/experimental/ittoryWebSocketSchema.ts
@@ -4,22 +4,29 @@ import {
 } from './TypeSafeWebSocket';
 
 export const ittoryRequestMapperDefinition = {
-  letter: {
-    channelMapper: (letterId: number) => `/topic/letter/${letterId}`,
-    requestMapper: (payload: { name: string; age: number }) => {
+  writeLetterElement: {
+    channelMapper: (letterId: number) => `/ws/letter/${letterId}/elements`,
+    requestMapper: (payload: { sequence: number; content: string }) => {
       return JSON.stringify(payload);
     },
   },
-  start: {
-    channelMapper: (letterId: number) => `/topic/letter/${letterId}/start`,
-    // requestMapper를 정의하지 않을 수도 있게. 사용하지 않기 때문.
+  startLetter: {
+    channelMapper: (letterId: number) => `/ws/letter/start/${letterId}`,
+  },
+  enterLetter: {
+    channelMapper: (letterId: number) => `/ws/letter/enter/${letterId}`,
+    requestMapper: (payload: { nickname: string }) => {
+      return JSON.stringify(payload);
+    },
+  },
+  endLetter: {
+    channelMapper: (letterId: number) => `/ws/letter/end/${letterId}`,
+  },
+  quitLetter: {
+    channelMapper: (letterId: number) => `/ws/letter/exit/${letterId}`,
   },
 } satisfies RequestMapperDefinition;
 
-// channel로 전달되는 raw response를 각 response로 변환하는 과정은 사용자가 직접 정의해야 함
-// Mapper + Switch-Case 구현 파트
-// TODO: 사용자가 (채널, 이벤트, 페이로드 타입)을 먼저 정의해놓고 제네릭으로 전달하면 자동 완성이 좀 될텐데 지금은 아예 안 되긴 함
-// 여기서 타입 힌트를 줘버리면 문제가 됨. 대신 satisfies를 쓰면 자동 완성은 되면서 타입 추론이 안 되는 문제도 해결됨
 export const userResponseMapperDefinition = {
   letter: {
     channelMapper: (letterId: number) => `/topic/letter/${letterId}`,
