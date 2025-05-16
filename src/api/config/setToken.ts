65ab098ffb8bef59d3d93de103350d8145b519e3
Seongbin Kim <seongbin9786@gmail.com>
2025-05-16 11:28:47 +0900

    Refactor: RL1M-248 userId localstorage 저장 로직 제거

--- src/api/config/setToken.ts
+++ src/api/config/setToken.ts
@@ -1,31 +1,5 @@
-import { jwtDecode } from 'jwt-decode';
-
 const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_KEY}&redirect_uri=${import.meta.env.VITE_LOGIN_REDIRECT}&response_type=code`;
 
 export const getKakaoCode = async () => {
   window.location.href = KAKAO_AUTH_URI;
-};
-
-// 세션에서 유저 아이디 받아오기
-export const getUserIdFromLocalStorage = () => {
-  const userId = window.localStorage.getItem('userId');
-  return userId;
-};
-
-interface jwtType {
-  exp: number;
-  iat: number;
-  role: string;
-  sub: string;
-  type: string;
-}
-
-// 세션에 유저 아이디 저장하기
-export const setUserIdFromAccessToken = (jwt: string) => {
-  const userId: jwtType = jwtDecode(jwt);
-  window.localStorage.setItem('userId', userId.sub);
-};
-
-export const setUserId = (userId: number | string) => {
-  window.localStorage.setItem('userId', userId.toString());
 };
