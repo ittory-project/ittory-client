0dd7679fff2e95183c924fa96c4d64df82353484
Seongbin Kim <seongbin9786@gmail.com>
2025-05-17 21:34:49 +0900

    HOTFIX: 30초로 임시 설정

--- src/hooks/useTimeLeft.ts
+++ src/hooks/useTimeLeft.ts
@@ -1,10 +1,15 @@
 import { useIntervalRerender } from './useIntervalRerender';
 
+const OFFICIAL_TIME_LIMIT = 30;
+
 // NOTE: write 쪽에서만 쓰이는 다소 local한 훅
 export const useTimeLeft = (startedAt: string | null) => {
   const timeDiff =
     new Date().getTime() - new Date(startedAt ?? new Date()).getTime();
-  const timeLeft = Math.max(0, 100 - Math.ceil(timeDiff / 1000));
+  const timeLeft = Math.max(
+    0,
+    OFFICIAL_TIME_LIMIT - Math.ceil(timeDiff / 1000),
+  );
 
   useIntervalRerender(true, 1000);
 
