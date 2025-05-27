3d8a7fb65282028c8b8edc132b3ff1cbe1e00fdf
Seongbin Kim <seongbin9786@gmail.com>
2025-05-27 14:26:57 +0900

    Fix: RL1M-341 카카오 로그인 리디렉션 시 Origin을 동적으로 설정하게

--- src/api/config/setToken.ts
+++ src/api/config/setToken.ts
@@ -1,4 +1,4 @@
-const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_KEY}&redirect_uri=${import.meta.env.VITE_LOGIN_REDIRECT}&response_type=code`;
+const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_KEY}&redirect_uri=${window.location.protocol}//${window.location.hostname}/login/redirect&response_type=code`;
 
 export const getKakaoCode = async () => {
   window.location.href = KAKAO_AUTH_URI;
