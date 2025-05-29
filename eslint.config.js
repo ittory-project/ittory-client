a9b97af6ebd48f3fc5b75d771b2d8e3780512056
Seongbin Kim <seongbin9786@gmail.com>
2025-05-29 15:57:43 +0900

    Chore: RL1M-350 Storybook 기본 설치

--- eslint.config.js
+++ eslint.config.js
@@ -4,59 +4,66 @@ import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import storybook from 'eslint-plugin-storybook';
 import unusedImports from 'eslint-plugin-unused-imports';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 // NOTE: tseslint.config를 [] 에 감싸서 내보내면 CLI가 제대로 동작하지 않음
-export default tseslint.config({
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...tanStackQuery.configs['flat/recommended'], // @see https://tanstack.com/query/v5/docs/eslint/eslint-plugin-query#recommended-setup
-    prettierRecommended,
-  ],
-  files: ['**/*.{js,jsx,ts,tsx}'],
-  ignores: ['dist', 'node_modules', 'public'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-    react,
-    'unused-imports': unusedImports,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react/react-in-tsx-scope': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+export default tseslint.config(
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tanStackQuery.configs['flat/recommended'], // @see https://tanstack.com/query/v5/docs/eslint/eslint-plugin-query#recommended-setup
+      prettierRecommended,
     ],
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
-    'no-multiple-empty-lines': 'error',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-        vars: 'all',
-        varsIgnorePattern: '^_',
-      },
-    ],
-    eqeqeq: 'error',
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['dist', 'node_modules', 'public'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      react,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-tsx-scope': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'no-multiple-empty-lines': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          vars: 'all',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      eqeqeq: 'error',
+    },
   },
-});
+  {
+    files: ['**/*.stories.{ts,tsx}'],
+    extends: [storybook.configs['flat/recommended']],
+  },
+);
