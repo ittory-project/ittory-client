61f99fbd64e67fdb7689b978adb196843f1ae9c7
Seongbin Kim <seongbin9786@gmail.com>
2025-06-12 13:16:35 +0900

    Feat: iOS Safe Area padding을 출력하게

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import MediaQuery from 'react-responsive';
 
 import '../App.css';
+import { printSafeArea } from '../utils';
 import { ExportLogButton } from './ExportLogButton';
 
 const isProdEnv = import.meta.env.VITE_DEPLOY_ENV === 'prod';
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    printSafeArea();
+  }, []);
+
   return (
     <div className="App safe-area">
       <MediaQuery minWidth={431}>
